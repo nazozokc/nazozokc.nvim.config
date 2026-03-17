39f5f290ce52f8043d09a64b7020764f687ee001
nazozokc <nazozokc@gmail.com>
2026-03-17 11:39:08 +0900

    fix: rename template dirs and remove invalid variables option

--- lua/plugins/misc/template.lua
+++ lua/plugins/misc/template.lua
@@ -6,15 +6,6 @@ return {
 			temp_dir = vim.fn.stdpath("config") .. "/template",
 			author = "nazozo",
 			email = "hoge@example.com",
-
-			variables = {
-				["_year_"] = function()
-					return os.date("%Y")
-				end,
-				["_date_"] = function()
-					return os.date("%Y-%m-%d")
-				end,
-			},
 		})
 	end,
 }
