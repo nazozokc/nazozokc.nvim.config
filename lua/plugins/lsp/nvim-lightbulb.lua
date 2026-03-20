bcd78dc9b887d69a08897d6042145aebcfbc5f58
nazozokc <nazozokc@gmail.com>
2026-03-20 16:58:45 +0900

    fix(deps): disable nvim-lightbulb (deprecated vim.validate signature)

--- lua/plugins/lsp/nvim-lightbulb.lua
+++ lua/plugins/lsp/nvim-lightbulb.lua
@@ -1,14 +1,4 @@
 return {
 	"kosayoda/nvim-lightbulb",
-	event = { "LspAttach" },
-	config = function()
-		require("nvim-lightbulb").setup({
-			autocmd = { enabled = true },
-			sign = { enabled = false },
-			virtual_text = {
-				enabled = true,
-				text = "💡",
-			},
-		})
-	end,
+	enabled = false,
 }
