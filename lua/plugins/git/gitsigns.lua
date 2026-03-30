12debc31e6e14c9cfa7292c3f65722ba47cc6bd0
nazozokc <nazozokc@gmail.com>
2026-03-30 11:37:48 +0900

    init

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -6,7 +6,7 @@ return {
 			signs = {
 				add = { text = "▎" },
 				change = { text = "▎" },
-				delete = { text = "▎" }, -- ← VSCodeは全部同じバー
+				delete = { text = "▎" }, -- すべて同じアイコンに統一
 				topdelete = { text = "▎" },
 				changedelete = { text = "▎" },
 			},
@@ -19,7 +19,7 @@ return {
 				follow_files = true,
 			},
 
-			current_line_blame = false, -- ← VSCodeはデフォOFF
+			current_line_blame = false, -- デフォルトで無効
 			word_diff = false,
 			update_debounce = 100,
 
@@ -28,7 +28,7 @@ return {
 			},
 		})
 
-		-- VSCode風キーマップ
+		-- キーマップ
 		vim.keymap.set("n", "]c", function()
 			require("gitsigns").next_hunk()
 		end)
--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -4,6 +4,7 @@ return {
 	keys = { { "<leader>ca", "<cmd>Lspsaga code_action<CR>" } },
 	branch = "main",
 	config = function()
+		-- デフォルト設定で使用
 		require("lspsaga").setup({})
 	end,
 }
